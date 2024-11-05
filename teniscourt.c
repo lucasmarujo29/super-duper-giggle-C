8486f7a7d51491ca6b9f52418c15553beabf86f0
lucasmarujo29 <lucas.bhmarinho2@gmail.com>
2024-11-05 02:19:38 -0300

    Update teniscourt.c

--- teniscourt.c
+++ teniscourt.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
 /*
+exercise: program that identifies whether a tennis ball was inside or outside the court in a given serve
+through the coordinate of the ball's contact and the court's limits
+
 a quadra é de (0,0) a (432,468)
 condição 1: 0<=xb<=432
 condição 2: 0<=yb<=468
 */
-
+#include <stdio.h>
 int main() {
     int xb, yb;
     scanf("%d%d",&xb,&yb);
